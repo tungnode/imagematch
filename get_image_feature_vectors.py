5b802067df4bae2d1f42a602d58bec0b738694b3
Tung <nguyenduytung.50cb@gmail.com>
2021-04-02 09:44:29 -0500

    catch exception

--- get_image_feature_vectors.py
+++ get_image_feature_vectors.py
@@ -10,6 +10,8 @@ import numpy as np
 # Glob for reading file names in a folder
 import glob
 import os.path
+
+
 #################################################
 #################################################
 # This function:
@@ -19,18 +21,20 @@ import os.path
 # Returns the pre processed image as 224 x 224 x 3 tensor
 #################################################
 def load_img(path):
-# Reads the image file and returns data type of string
-  img = tf.io.read_file(path)
-  # Decodes the image to W x H x 3 shape tensor with type of uint8
-  img = tf.io.decode_jpeg(img, channels=3)
-  # Resizes the image to 224 x 224 x 3 shape tensor
-  img = tf.image.resize_with_pad(img, 224, 224)
-  # Converts the data type of uint8 to float32 by adding a new axis
-  # img becomes 1 x 224 x 224 x 3 tensor with data type of float32
-  # This is required for the mobilenet model we are using
-  img = tf.image.convert_image_dtype(img,tf.float32)[tf.newaxis, ...]
- 
-  return img
+    # Reads the image file and returns data type of string
+    img = tf.io.read_file(path)
+    # Decodes the image to W x H x 3 shape tensor with type of uint8
+    img = tf.io.decode_jpeg(img, channels=3)
+    # Resizes the image to 224 x 224 x 3 shape tensor
+    img = tf.image.resize_with_pad(img, 224, 224)
+    # Converts the data type of uint8 to float32 by adding a new axis
+    # img becomes 1 x 224 x 224 x 3 tensor with data type of float32
+    # This is required for the mobilenet model we are using
+    img = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+
+    return img
+
+
 #################################################
 # This function:
 # Loads the mobilenet model in TF.HUB
@@ -38,28 +42,32 @@ def load_img(path):
 # Saves each of the feature vectors in a file
 #################################################
 def get_image_feature_vectors():
- 
- # Definition of module with using tfhub.dev
- module_handle = "https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/feature_vector/4"
- # Loads the module
- module = hub.load(module_handle)
-# Loops through all images in a local folder
- files = glob.glob('..\\copyHunter\\scripts\\images\\*.jpeg')
- files.extend(glob.glob('..\\copyHunter\\scripts\\images\\*.png'))  
- files.extend(glob.glob('..\\copyHunter\\scripts\\images\\*.jpg'))
- for filename in files:
-  print(filename)
-# Loads and pre-process the image
-  img = load_img(filename)
-# Calculate the image feature vector of the img
-  features = module(img)
-# Remove single-dimensional entries from the 'features' array  
-  feature_set = np.squeeze(features)
- 
-  # Saves the image feature vectors into a file for later use
-  outfile_name = os.path.basename(filename) + ".npz"
- 
-  out_path = os.path.join('.\\vectors\\',outfile_name)
-# Saves the 'feature_set' to a text file
-  np.savetxt(out_path, feature_set, delimiter=',')
+    # Definition of module with using tfhub.dev
+    module_handle = "https://tfhub.dev/google/imagenet/mobilenet_v2_140_224/feature_vector/4"
+    # Loads the module
+    module = hub.load(module_handle)
+    # Loops through all images in a local folder
+    files = glob.glob('..\\copyHunter\\scripts\\images\\*.jpeg')
+    files.extend(glob.glob('..\\copyHunter\\scripts\\images\\*.png'))
+    files.extend(glob.glob('..\\copyHunter\\scripts\\images\\*.jpg'))
+    for filename in files:
+        try:
+            print(filename)
+            # Loads and pre-process the image
+            img = load_img(filename)
+            # Calculate the image feature vector of the img
+            features = module(img)
+            # Remove single-dimensional entries from the 'features' array
+            feature_set = np.squeeze(features)
+
+            # Saves the image feature vectors into a file for later use
+            outfile_name = os.path.basename(filename) + ".npz"
+
+            out_path = os.path.join('.\\vectors\\', outfile_name)
+            # Saves the 'feature_set' to a text file
+            np.savetxt(out_path, feature_set, delimiter=',')
+        except (Exception):
+            pass
+
+
 get_image_feature_vectors()
