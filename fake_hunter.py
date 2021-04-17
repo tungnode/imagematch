ab00d587256981549abc9cbcc6921204bf403dc6
Tung <nguyenduytung.50cb@gmail.com>
2021-04-17 18:29:53 -0500

    combine nmbslib and annoy

--- fake_hunter.py
+++ fake_hunter.py
@@ -4,8 +4,6 @@ import time
 import numpy as np
 import threading
 import glob
-# import tensorflow as tf
-# from tensorflow.python.client import timeline
 from scipy import spatial
 import multiprocessing
 import random
@@ -15,6 +13,7 @@ from multiprocessing.managers import SharedMemoryManager
 import json
 import os.path
 import logging
+from annoy import AnnoyIndex
 logging.basicConfig(level=logging.DEBUG)
 
 def load_glob_worker(all_files,file_name_index,data_set,start,end,lock):
@@ -46,7 +45,7 @@ def load_glob_worker(all_files,file_name_index,data_set,start,end,lock):
 #         file_index[index] = all_files[index]
 #         all_vectors_data_set.append(np.loadtxt(all_files[index]))
 
-def is_legit_token(master_file_name,neighbor_file_name,map_of_token_with_owners):
+def is_legit_token(master_file_name,neighbor_file_name):
     try:
         master_token = master_file_name.split("_")[0]
         neighbor_token = neighbor_file_name.split("_")[0]
@@ -63,17 +62,66 @@ def is_legit_token(master_file_name,neighbor_file_name,map_of_token_with_owners)
     return False
 
 
-def is_already_exist(master_file_name,neighbor_file_name,neighbor_master_set):
+def is_already_exist(master_file_name,neighbor_file_name):
     new_neighbor_master_set = frozenset([master_file_name,neighbor_file_name])
     if new_neighbor_master_set not in neighbor_master_set:
         neighbor_master_set[new_neighbor_master_set] = new_neighbor_master_set
         return False
     else:
         return True
+def annoy_similarity(file_index_to_file_name,file_index_to_file_vector):
+    dims = 1792
+    n_nearest_neighbors = 20
+    trees = 10000
+    t = AnnoyIndex(dims, metric='angular')
+    for k in file_index_to_file_vector:
+        t.add_item(k, file_index_to_file_vector[k])
+    t.build(trees)
+
+    
+    all_nearest_neighboor = []
+    # Loops through all indexed items
+    for i in file_index_to_file_name.keys():
+        # Assigns master file_name, image feature vectors
+        # and product id values
+        master_file_name =  os.path.basename(file_index_to_file_name[i]).split('.')[0]
+        master_vector = file_index_to_file_vector[i]
+        # Calculates the nearest neighbors of the master item
+        nearest_neighbors = t.get_nns_by_item(i, n_nearest_neighbors)
+        # Loops through the nearest neighbors of the master item
+        for j in nearest_neighbors:
+            print(j)
+            # Assigns file_name, image feature vectors and
+            # product id values of the similar item
+            neighbor_file_name = os.path.basename(file_index_to_file_name[j]).split('.')[0]
+            neighbor_file_vector = file_index_to_file_vector[j]
+
+            # Calculates the similarity score of the similar item
+            similarity = 1 - spatial.distance.cosine(master_vector, neighbor_file_vector)
+            rounded_similarity = int((similarity * 10000)) / 10000.0
+
+            # Appends master product id with the similarity score
+            # and the product id of the similar items
+            if ((rounded_similarity > 0.98) and (master_file_name != neighbor_file_name) 
+                and (is_legit_token(master_file_name,neighbor_file_name) == False)
+                and (is_already_exist(master_file_name,neighbor_file_name) == False)):
+                    all_nearest_neighboor.append({
+                            'similarity': rounded_similarity,
+                            'master_pi': master_file_name,
+                            'similar_pi': neighbor_file_name})
+
+        print("---------------------------------")
+        print("Similarity index       : %s" % i)
+        print("Master Image file name : %s" % file_index_to_file_name[i])
+        print("Nearest Neighbors.     : %s" % nearest_neighbors)
+        print("--- %.2f minutes passed ---------" % ((time.time() - start_time) / 60))
+    return all_nearest_neighboor
+
+
 
 with open('..\\copyHunter\\scripts\\owners_data.json') as json_file:
     map_of_token_with_owners = json.load(json_file)
-neighbor_master_map = {}
+neighbor_master_set = {}
 if __name__ == '__main__':
     # load image vector files
     all_files = glob.glob('.\\vectors\\*.npz')
@@ -84,7 +132,7 @@ if __name__ == '__main__':
     data_set = []# manager.list()
     file_name_index = {}#manager.dict()
     start_time = time.time()
-    named_nearest_neighbors = []
+    
     
  
     # number_of_processes = 5
@@ -120,7 +168,12 @@ if __name__ == '__main__':
    
     # get all nearest neighbours for all the datapoint
     # using a pool of 4 threads to compute
+    # Defining data structures as empty dict
+    annoy_file_index_to_file_name = {}
+    annoy_file_index_to_file_vector = {}
+
     all_nearest_data = index.knnQueryBatch(data_set, k=10, num_threads=4)
+    named_nearest_neighbors = []
     for master_index, master in enumerate(all_nearest_data):
         master_neighbors_index =master[0]
         master_neighbors_distances = master[1]
@@ -140,12 +193,16 @@ if __name__ == '__main__':
                                     # 'spatial_similarity': str(rounded_similarity),
                                     'nmslib_similarity': str(distance_to_neighbor),
                                     'master_pi': master_file_path,
-                                    'similar_pi': neighbor_file_path})    
-
+                                    'similar_pi': neighbor_file_path})
+                        annoy_file_index_to_file_vector[master_index] = data_set[master_index]
+                        annoy_file_index_to_file_name[master_index] = master_file_path
+                        annoy_file_index_to_file_vector[neighbor_index] = data_set[neighbor_index]
+                        annoy_file_index_to_file_name[neighbor_index] = neighbor_file_path
+    annoy_nearest_neighbor = annoy_similarity(annoy_file_index_to_file_name,annoy_file_index_to_file_vector)
     print("Step.2 - Similarity score calculation - Finished ")
     # Writes the 'named_nearest_neighbors' to a json file
     with open('nearest_neighbors.json', 'w') as out:
-        json.dump(list(named_nearest_neighbors), out)
+        json.dump(list(annoy_nearest_neighbor), out)
     print("Step.3 - Data stored in 'nearest_neighbors.json' file ")
     print("--- Prosess completed in %.2f minutes ---------" %
         ((time.time() - start_time) / 60))
